137ca1dfa8f99a56f5118bbbcdeeecd9b30867fb
Glenn Key <glennkey78@live.no>
2021-09-24 23:30:10 +0200

    added gloabl style delete and modified the component styles

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import State from './compoments/state/State';
 import Button from './compoments/button/Button';
+import './style/global.scss';
 
 
 function App() {
--- src/compoments/button/Button.jsx
+++ src/compoments/button/Button.jsx
@@ -8,31 +8,29 @@ function Button() {
     const cash = useSelector(state => state.cash);
     const dispatch = useDispatch();
 
-    return (
-        <>
-            <div className={styles.random}>
-                <div>
-                    <label className="label">Gather: </label>
-                    <span>Cost: 1 day </span>
-                    <button onClick={() => dispatch(gather())}>Gather</button>
-                </div>
-                <div>
-                    <label>Travel: </label>
-                    <span>Cost -20 supplies & +1 day </span>
-                    {supplies > 0 ? <button onClick={() => dispatch(travel(1))}>Travel</button> : <button onClick={() => dispatch(travel(1))} style={{color: 'red'}}>Travel (not enough supplies)</button>}
-                </div>
-                <div>
-                    <label>Buy 25 supplies: </label>
-                    <span>Cost 15 cash </span>
-                    {cash >= 0 ? <button onClick={() => dispatch(buy())}>Buy</button> : <button onClick={() => dispatch(buy())} style={{color: 'red'}}>Buy (not enough cash)</button> }
-                </div>
-                <div>
-                    <label>Sell supplies +5 cash</label>
-                    <span>Cost 20 supplies </span>
-                    {supplies > 20 ? <button onClick={() => dispatch(sell())}>Sell supplies for cash</button> : <button onClick={() => dispatch(sell())} style={{color: 'red'}}>Sell (not enough supplies)</button>}
-                </div>
+    return ( 
+        <div className={styles.container}>
+            <div className={styles.actionContainer}>
+                <label className={styles.label}>Gather: </label>
+                <span>Cost: 1 day </span>
+                <button onClick={() => dispatch(gather())} className={styles.btn}>Gather</button>
             </div>
-        </>
+            <div className={styles.actionContainer}>
+                <label className={styles.label}>Travel: </label>
+                <span>Cost -20 supplies & +1 day </span>
+                {supplies > 0 ? <button onClick={() => dispatch(travel(1))} className={styles.btn}>Travel</button> : <button onClick={() => dispatch(travel(1))} style={{color: 'red'}} className={styles.btn}>Travel (not enough supplies)</button>}
+            </div>
+            <div className={styles.actionContainer}>
+                <label className={styles.label}>Buy 25 supplies: </label>
+                <span>Cost 15 cash </span>
+                {cash >= 0 ? <button onClick={() => dispatch(buy())} className={styles.btn}>Buy</button> : <button onClick={() => dispatch(buy())} style={{color: 'red'}} className={styles.btn}>Buy (not enough cash)</button> }
+            </div>
+            <div className={styles.actionContainer}>
+                <label className={styles.label}>Sell supplies for +5 cash: </label>
+                <span>Cost 20 supplies </span>
+                {supplies > 20 ? <button onClick={() => dispatch(sell())} className={styles.btn}>Sell</button> : <button onClick={() => dispatch(sell())} style={{color: 'red'}} className={styles.btn}>Sell (not enough supplies)</button>}
+            </div>
+        </div>
     )
 }
 
